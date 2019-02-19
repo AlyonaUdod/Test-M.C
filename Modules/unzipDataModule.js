68ab893959be6d2fc19f45e0473f13c7a840e77a
Your Name <freelspringisoul@gmail.com>
2019-02-19 14:04:40 +0200

    add_zip/folder_start

--- Modules/unzipDataModule.js
+++ Modules/unzipDataModule.js
@@ -2,10 +2,10 @@ const filesToConvert = require('./filesToConvertModule');
 const decompress = require('decompress');
 const decompressUnzip = require('decompress-unzip');
 
-const path = process.cwd();
-let inputDataPath = '';
-
 module.exports.unzipData = function(inputZip, outputZipFolder, resultFileName){
+    const path = process.cwd();
+    let inputDataPath = '';
+    
     decompress(inputZip, outputZipFolder, {
         plugins: [
             decompressUnzip()
--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const unzip = require('./Modules/unzipDataModule');
+const filesToConvert = require('./Modules/filesToConvertModule');
 
 const inputZip = 'Carrier_Integration_-_Data.zip';
+const inputFolderWithCSV = nullz;
 const outputZipFolder = 'original_data';
 const resultFileName = 'result';
 
-unzip.unzipData(inputZip, outputZipFolder, resultFileName)
+if(inputZip){
+    unzip.unzipData(inputZip, outputZipFolder, resultFileName);
+} else if (inputFolderWithCSV) {
+    const path = process.cwd();
+    let inputDataPath = `${path}/${inputFolderWithCSV}`;
+    filesToConvert.getFilesFromDir(inputDataPath, resultFileName);
+};  
